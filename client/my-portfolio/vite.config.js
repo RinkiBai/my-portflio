b3b3b06d795811a809993970716f20a30a5c5a51
Rinki Bai <rinkirinki4349@gmail.com>
2025-05-21 16:49:18 +0530

    change vite.config.js

--- client/my-portfolio/vite.config.js
+++ client/my-portfolio/vite.config.js
@@ -1,19 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://portfolio-mern-boij.onrender.com',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
@@ -33,7 +27,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
+      '@': resolve(__dirname, 'src')
     }
   }
 })
