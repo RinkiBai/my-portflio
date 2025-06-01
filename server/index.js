8d403cd2115ea3a728eee46a65597626d9453705
Rinki Bai <rinkirinki4349@gmail.com>
2025-06-01 13:00:46 +0530

    update index.js

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ dotenv.config();
 const app = express();
 
 const allowedOrigins = [
+  'https://portfolio-mern-3kihturi8-rinki-bais-projects.vercel.app',
   'https://portfolio-mern-gct8-rinki-bais-projects.vercel.app',
   process.env.FRONTEND_URL, // pulls https://portfolio-mern-gct8.vercel.app from .env
 ];
